37c22afe3ad3e17295f84264656f8808e4d5a66c
Lauri44 <53011140+Lauri44@users.noreply.github.com>
2022-05-02 14:20:21 +0200

    Add files via upload

---
+++ C# - TextStream-Textdateien (Infoblatt und Beispielprogramm)/Textdateien mit FileStream/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Textdateien_mit_FileStream
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSchreiben = new System.Windows.Forms.Button();
+            this.txtSchreiben = new System.Windows.Forms.TextBox();
+            this.btnLesen = new System.Windows.Forms.Button();
+            this.lstAusgabe = new System.Windows.Forms.ListBox();
+            this.btnBeenden = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSchreiben
+            // 
+            this.btnSchreiben.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSchreiben.Location = new System.Drawing.Point(31, 99);
+            this.btnSchreiben.Name = "btnSchreiben";
+            this.btnSchreiben.Size = new System.Drawing.Size(163, 45);
+            this.btnSchreiben.TabIndex = 0;
+            this.btnSchreiben.Text = "Zeile schreiben";
+            this.btnSchreiben.UseVisualStyleBackColor = true;
+            this.btnSchreiben.Click += new System.EventHandler(this.btnSchreiben_Click);
+            // 
+            // txtSchreiben
+            // 
+            this.txtSchreiben.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSchreiben.Location = new System.Drawing.Point(31, 60);
+            this.txtSchreiben.Name = "txtSchreiben";
+            this.txtSchreiben.Size = new System.Drawing.Size(163, 26);
+            this.txtSchreiben.TabIndex = 2;
+            // 
+            // btnLesen
+            // 
+            this.btnLesen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLesen.Location = new System.Drawing.Point(31, 270);
+            this.btnLesen.Name = "btnLesen";
+            this.btnLesen.Size = new System.Drawing.Size(163, 47);
+            this.btnLesen.TabIndex = 3;
+            this.btnLesen.Text = "Alle Zeilen lesen";
+            this.btnLesen.UseVisualStyleBackColor = true;
+            this.btnLesen.Click += new System.EventHandler(this.btnLesen_Click);
+            // 
+            // lstAusgabe
+            // 
+            this.lstAusgabe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstAusgabe.FormattingEnabled = true;
+            this.lstAusgabe.ItemHeight = 20;
+            this.lstAusgabe.Location = new System.Drawing.Point(236, 33);
+            this.lstAusgabe.Name = "lstAusgabe";
+            this.lstAusgabe.Size = new System.Drawing.Size(246, 284);
+            this.lstAusgabe.TabIndex = 4;
+            // 
+            // btnBeenden
+            // 
+            this.btnBeenden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBeenden.Location = new System.Drawing.Point(333, 346);
+            this.btnBeenden.Name = "btnBeenden";
+            this.btnBeenden.Size = new System.Drawing.Size(163, 47);
+            this.btnBeenden.TabIndex = 5;
+            this.btnBeenden.Text = "Beenden";
+            this.btnBeenden.UseVisualStyleBackColor = true;
+            this.btnBeenden.Click += new System.EventHandler(this.btnBeenden_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hier Text eingeben:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBeenden);
+            this.Controls.Add(this.lstAusgabe);
+            this.Controls.Add(this.btnLesen);
+            this.Controls.Add(this.txtSchreiben);
+            this.Controls.Add(this.btnSchreiben);
+            this.Name = "Form1";
+            this.Text = "Schreiben und Lesen von Textdateien";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSchreiben;
+        private System.Windows.Forms.TextBox txtSchreiben;
+        private System.Windows.Forms.Button btnLesen;
+        private System.Windows.Forms.ListBox lstAusgabe;
+        private System.Windows.Forms.Button btnBeenden;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C# - TextStream-Textdateien (Infoblatt und Beispielprogramm)/Textdateien mit FileStream/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Textdateien_mit_FileStream
+{
+    public partial class Form1 : Form
+    {
+        // Form - globale Variablen und Objekte:
+        FileStream strom;   // Objektvariable; Instanzierung erfolgt später...!
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSchreiben_Click(object sender, EventArgs e)
+        {
+            // Objekt (=Instanz) "strom" der FileStream - Klasse erstellen.
+            // Übergaben: 1.) Name der Textdatei, 
+            // 2.) Modus (hier: Append - zum ERSTELLEN der Textdatei, wenn noch 
+            // nicht vorhanden, und zum ANFÜGEN (Append)von Textzeilen an eine 
+            // schon bestehende Textdatei, (ÜBERSCHREIBEN? -- mit: "FileMode.Create"!)
+            // 3.) Schreibzugriff herstellen.
+            strom = new FileStream("Test.txt", FileMode.Append, FileAccess.Write);
+            
+            // Objekt (=Instanz) "schreiben" der StreamWriter - Klasse erstellen.
+            // mit Verweis auf den übergeordneten FileStream "strom".
+            StreamWriter schreiben = new StreamWriter(strom);
+
+            // Zeile aus txtSchreiben übernehmen und in die Textdatei schreiben.
+            schreiben.WriteLine(txtSchreiben.Text);
+
+            // Nach beendigtem Schreibvorgang wieder schließen des StreamWriters.
+            schreiben.Close();
+        }
+
+
+        private void btnBeenden_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void btnLesen_Click(object sender, EventArgs e)
+        {
+            // Wie oben; jedoch hier: FileMode.Open zum Öffnen einer bestehenden 
+            // Textdatei und FileAccess.Read zum Lesen der Daten aus der Textdatei.
+            strom = new FileStream("Test.txt", FileMode.Open, FileAccess.Read);
+
+            // Objekt (=Instanz) "lesen" der StreamReader - Klasse erstellen.
+            // mit Verweis auf den übergeordneten FileStream "strom".
+            StreamReader lesen = new StreamReader(strom);
+
+            string zeile;
+
+            // Solange weitere Daten vorhanden sind --> Einlesen und anzeigen.
+            lstAusgabe.Items.Clear();
+            while (lesen.Peek() != -1)
+            {
+                // Zeilen aus der Textdatei einlesen und in lstAusgabe anzeigen.
+                zeile = lesen.ReadLine();
+                lstAusgabe.Items.Add(zeile);
+            }
+
+            // Nach beendigtem Lesevorgang wieder schließen des StreamReaders.
+            lesen.Close();
+        }
+    }
+}
