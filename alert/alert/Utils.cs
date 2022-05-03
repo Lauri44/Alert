977d9db72312031c5650a7f8346778a519ec6bde
Lauri44 <53011140+Lauri44@users.noreply.github.com>
2022-05-03 08:04:23 +0200

    Add files via upload

---
+++ alert/alert/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace utils
+{
+    static class Timer
+    {
+        // Since WPF has no Timer itself i created my own one
+
+        static public int interval { get; set; }
+        static private bool run = false;
+        static public void start(Action func)
+        {
+            //Task timer = new Task(() => tick(func));
+            //timer.Start();
+            tick(func);
+            run = true;
+        }
+
+        static public void stop()
+        {
+            run = false;
+        }
+
+        static private void tick(Action func)
+        {
+            while (run)
+            {
+                func();
+                Task.Delay(interval);
+            }
+            
+        }
+    }
+}
