3f58504b118cccb396f43e7eceb47f3ef2c4b03e
Lauri44 <53011140+Lauri44@users.noreply.github.com>
2022-05-03 14:34:08 +0200

    Add files via upload

--- alert/alert/Utils.cs
+++ alert/alert/Utils.cs
@@ -7,25 +7,40 @@ using System.Threading;
 
 namespace utils
 {
+    /// <summary>
+    /// Timer
+    /// </summary>
     static class Timer
     {
-        // Since WPF has no Timer itself i created my own one
+        // Since WPF has no Timer itself i created my own
 
-        static public int interval { get; set; }
-        static private bool run = false;
-        static public void start(Action func)
+        static private int interval;
+        static public bool run { get; } = false;
+
+        /// <summary>
+        /// Starts the timer. <br/>
+        /// Takes a function as a argument and runs it in certain intervals. <br/>
+        /// Takes int as argument vor the interval (1000 = 1000ms = 1s). Default time is 1000
+        /// </summary>
+        /// <param name="func"></param>
+        static public void start(Action func, int interval = 1000)
         {
+            this.interval = interval;
             //Task timer = new Task(() => tick(func));
             //timer.Start();
             tick(func);
             run = true;
         }
 
+        /// <summary>
+        /// Stops the timer
+        /// </summary>
         static public void stop()
         {
             run = false;
         }
 
+        // It's about the tick... IT'S ALWAYS ABOUT THE TICK!!!
         static private void tick(Action func)
         {
             while (run)
