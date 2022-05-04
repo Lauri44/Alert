6191593cf4008a79660ada99d16715e7a525b015
Lauri44 <53011140+Lauri44@users.noreply.github.com>
2022-05-04 22:41:17 +0200

    Add files via upload

---
+++ alert/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace alert
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static private List<int> signalList = new List<int>();
+
+        static public void signalCheck()
+        {
+            for(int signal = 1; signal <= 4; signal++)
+            {
+                if (api.ReadDigitalChannel(signal) && !signalList.Contains(signal))
+                {
+                    signalList.Add(signal);
+                }
+                else if(!api.ReadDigitalChannel(signal) && signalList.Contains(signal))
+                {
+                    signalList.Remove(signal);
+                }
+            }
+        }
+    }
+}
